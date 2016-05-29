fe38ae72a4e9e48012aedb517163355ed5283e6e
Bill Stiber <billstiber@gmail.com>
2016-05-28 21:01:49 -0400

    add new files, arrays and hashes

---
+++ arrays/hashes.rb
@@ -0,0 +1,8 @@
+list = {"friends" => "dave", "enemies" => "james"}
+favorite_foods = {:chocolate => "desert", :dinner => "fried chicken"}
+
+# to add items to a Hash use the name and []
+list["friends"] = "jessica"
+
+# treehouse challenge
+grocery_item_1 = {"name" => "milk"}
---
+++ arrays/pop.rb
@@ -0,0 +1,10 @@
+# Set a variable called last_item which uses the pop method to take the last
+# item out of the grocery_list array.
+
+grocery_list = ["milk", "eggs", "bread", "ice cream", "carrots", "potatoes"]
+last_item = grocery_list.pop
+
+# Set a variable called first_item which uses the shift method to take the first
+# item out of the grocery_list array.
+
+first_item = grocery_list.shift
