da61b8bcb141de75bfb5ec22ed14711c732efa6d
Bill Stiber <billstiber@gmail.com>
2016-05-30 17:49:50 -0400

    add changes to file

--- loops/while_number.rb
+++ loops/while_number.rb
@@ -10,7 +10,7 @@ end
 
 answer = 0
 while answer < 5
-  puts "how many times do you want to print hello?  Enter a number from 1 to 5."
+  puts "how many times do you want to print hello?  Enter a number from 1 to 5. Enter a number greater than 5 to exit"
   answer = gets.chomp.to_i
   print_hello(answer)
 end
