564c2af2d1ff190ca83cad1f2797e0ca0a7881f8
Bill Stiber <billstiber@gmail.com>
2016-05-29 22:33:41 -0400

    add file loop.rb

---
+++ loops/loop.rb
@@ -0,0 +1,29 @@
+# loop exercises ===> Using the loop construct, add the current value of number to the
+# numbers array. Inside of the loop, add 1 to the number variable. Use the break keyword
+# to exit the loop once the numbers array has 3 items.
+
+numbers = []
+number = 0
+
+# loop do
+#   numbers.push(number)
+#   if numbers.length == 3
+#     break
+#   end
+#   number += 1
+# end
+#
+# puts numbers
+
+
+numbers = []
+number = 0
+
+loop do
+ numbers.push(number)
+ if numbers.length == 3
+   break
+  end
+  number += 1
+end
+puts numbers
