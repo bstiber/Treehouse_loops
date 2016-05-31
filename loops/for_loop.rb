3dfbc16d35af17a932847d37629a9e606675fff6
Bill Stiber <billstiber@gmail.com>
2016-05-30 21:47:21 -0400

    add file

---
+++ loops/for_loop.rb
@@ -0,0 +1,13 @@
+# for_loop.rb practice
+
+for item in 1..5 do
+  puts "this is item number #{item}."
+end
+
+
+# practice using for loops with arrays, NOTICE there is no "do" nor 1..3 "number"
+# this print each array out in a line
+
+for array in ["majic", "maker", "hands"]
+  puts "You got the #{array}."
+end
