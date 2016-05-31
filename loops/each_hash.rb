71f063f6854abae9b55bda493d07fdba330dc1ad
Bill Stiber <billstiber@gmail.com>
2016-05-30 21:04:38 -0400

    add code to each_hash.rb file

--- loops/each_hash.rb
+++ loops/each_hash.rb
@@ -1,3 +1,4 @@
+# iteration over hashes both using key and values as arguments
 
 business = {"name" => "Treehouse", "location" => "Portland, OR"}
 women = {"hair color" => "blonde", "body" => "petite"}
@@ -6,10 +7,18 @@ women.each do |key, value|
   puts "The Key is #{key}, and the Value is #{value}"
 end
 
-p women
 
 business.each do |key, value|
   puts "Key is : #{key}, Value is : #{value}"
 end
 
-p business
+# iteration over hashes using each_key, and each_value to iterated over just the
+# key or the value
+
+women.each_key do |key|
+  puts "Key is #{key}."
+end
+
+business.each_value do |value|
+  puts "Value is #{value}."
+end
